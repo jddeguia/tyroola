d5b59f376c6ae61fe4ddbd6097b04f930beba8ac
jddeguia <justin.dayaodeguia@gmail.com>
2025-05-02 01:06:43 +0800

    add more metadata

--- tyroola_dataplatform/models/base/base_funnel_analysis.sql
+++ tyroola_dataplatform/models/base/base_funnel_analysis.sql
@@ -3,16 +3,32 @@
 WITH base AS (
     SELECT
         DATE(FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP_MICROS(event_timestamp))) AS event_date,
-        event_name,
+        FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP_MICROS(event_timestamp)) AS event_timestamp,
+        user_pseudo_id,
+        (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS ga_session_id,
+        event_name,  
+        device.category AS device_category,
         items.item_name AS product_name,
         items.item_brand AS brand,
+        items.item_category AS item_category,
+        ecommerce.total_item_quantity,
+        ecommerce.purchase_revenue_in_usd,
+        ecommerce.shipping_value_in_usd,
+        ecommerce.refund_value_in_usd,
+        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'payment_type') AS payment_type,
         (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'source') AS utm_source,
         (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'medium') AS utm_medium,
-        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'campaign') AS utm_campaign
+        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'campaign') AS utm_campaign,
     FROM {{ source('public_dataset', 'events_*') }},
     UNNEST(items) AS items
     WHERE event_name IN ('view_item', 'add_to_cart', 'begin_checkout', 'purchase')
+),
+
+session_id AS (
+    SELECT *,
+        CONCAT(user_pseudo_id, CAST(ga_session_id AS STRING)) AS session_id
+    FROM base
 )
 
-SELECT * FROM base
+SELECT * FROM session_id
 
--- tyroola_dataplatform/models/mart/mart_agg_funnel_analysis.sql
+++ tyroola_dataplatform/models/mart/mart_agg_funnel_analysis.sql
@@ -11,24 +11,47 @@ WITH mart AS (
     FROM {{ ref('mart_funnel_analysis') }}
 ),
 
-aggregates AS (
-    SELECT 
-        event_date,
-        event_name,
-        product_name,
-        brand,
-        utm_source,
-        utm_medium,
-        utm_campaign,
+funnel_step AS (
+    SELECT *,
+        CASE 
+            WHEN event_name = 'view_item' THEN 1
+            WHEN event_name = 'add_to_cart' THEN 2
+            WHEN event_name = 'begin_checkout' THEN 3
+            WHEN event_name = 'purchase' THEN 4
+            ELSE 0
+        END AS funnel_step 
+    FROM mart
+),
+
+minutes_to_next_event AS (
+    SELECT *,
+        TIMESTAMP_DIFF(
+            LEAD(TIMESTAMP(event_timestamp)) OVER (PARTITION BY user_pseudo_id, session_id ORDER BY event_timestamp),
+            TIMESTAMP(event_timestamp),
+            MINUTE
+        ) AS minutes_to_next_event,
+    FROM funnel_step
+),
+
+event_counts AS (
+    SELECT *,
         SUM(CASE WHEN event_name = 'view_item' THEN 1 ELSE 0 END) AS users_viewed_product,
         SUM(CASE WHEN event_name = 'add_to_cart' THEN 1 ELSE 0 END) AS users_added_to_cart,
         SUM(CASE WHEN event_name = 'begin_checkout' THEN 1 ELSE 0 END) AS users_started_checkout,
         SUM(CASE WHEN event_name = 'purchase' THEN 1 ELSE 0 END) AS users_completed_purchase
-    FROM mart
+    FROM minutes_to_next_event
+    GROUP BY ALL
+),
+
+time_metrics AS (
+    SELECT *,
+        AVG(CASE WHEN funnel_step = 1 THEN minutes_to_next_event END) AS avg_view_to_cart_minutes,
+        AVG(CASE WHEN funnel_step = 2 THEN minutes_to_next_event END) AS avg_cart_to_checkout_minutes,
+        AVG(CASE WHEN funnel_step = 3 THEN minutes_to_next_event END) AS avg_checkout_to_purchase_minutes,
+    FROM event_counts
     GROUP BY ALL
 )
 
-SELECT * FROM aggregates
-
+SELECT * FROM time_metrics
         
 
